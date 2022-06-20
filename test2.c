ca5a81e0b368177732eff3766b8104bdc1fe7413
Mark_LongND <ndlong@humaxdigital.com>
2022-06-20 09:47:41 +0700

    update test1,2

--- test2.c
+++ test2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void main () {
 	char showMe[] = "HUMAX"; // bao gom ki tu NULL ket thuc 
 	int foo = 0;
 	int bar = 0;
 	
-	printf("%s", (char*)&foo-);
+	printf("%s\n", (char*)&foo+10);
+	printf("%s\n", (char*)&bar+6);
 }
