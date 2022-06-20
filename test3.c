00bbbe473e7bbd6f0c90f7c43241b25bd26ef8a1
Mark_LongND <ndlong@humaxdigital.com>
2022-06-20 16:48:06 +0700

    update-all

--- test3.c
+++ test3.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
 struct _command {
-	int *(callback)(int);
-	struct _command *next;
+	int (*callback)(int); // A command will excecute a callback function
+	struct _command* next;
 };
-
-struct _command * initCommandList();
-void insertCommand(command * list, int *(callback)(int));
-
-int main () {
-	int add5(int a) {
-		return a + 5;
+typedef struct _command command;
+struct _command* initCommandList() ;
+command* lastCommand;
+command* orderOfFunctions;
+int A;
+void insertCommand(command** list , int (*callback)(int))
+{
+	int i;
+	command* func = (command*)malloc(sizeof(command));
+	func->next = NULL;
+	func->callback = callback;
+	command* temp = *list;
+	if (*list == NULL) *list = func, lastCommand = func;
+	else
+	{
+		lastCommand->next = func;
+		lastCommand = func;
 	}
-	int sub4(int a) {
-		return a - 4;
+}
+int calculate(int a, struct _command** orderOfFunctions)
+{
+	A = a;
+	command* temp = *orderOfFunctions;
+	while (temp->next != NULL)
+	{
+		A = temp->callback(A);
+		temp = temp->next;
 	}
-	int mul3 (int a) {
-		return a * 3;
-	}
-	int div2 (int a) {
-		return a / 2;
-	}
+	return temp->callback(A);
+}
+int add5(int a) { return (a + 5); };
+int sub4(int a) { return (a - 4); };
+int mul3(int a) { return (a * 3); };
+int div2(int a) { return (a / 2); };
+int main()
+{
+	insertCommand(&orderOfFunctions, add5);
+	insertCommand(&orderOfFunctions, sub4);
+	insertCommand(&orderOfFunctions, mul3);
+	insertCommand(&orderOfFunctions, div2);
+	printf("%d", calculate(5, &orderOfFunctions));
 }
--- test5.c
+++ test5.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (int argc. char *argv[]) {
+int main (int argc, char *argv[]) {
 	int (*fp)(char *);
 	
 	if(argc!=2) {
 		printf("insert 0xaddress\n");
 		exit(-1);
 	}
-	
-	
+
 	if((int(*)(char *))&puts == fp) {
 		fp("Well done! You had 'puts'. Executed it for you :)");
 	}
